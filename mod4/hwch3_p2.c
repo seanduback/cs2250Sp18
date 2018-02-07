e244f193cb6b5a99a5cc9ae06b3e184af6a9a949
seanduback <seandubck@mail.weber.edu>
2018-02-06 18:29:43 -0700

    Finished hw3_p2

--- mod4/hwch3_p2.c
+++ mod4/hwch3_p2.c
@@ -28,12 +28,14 @@ int main()
     char tire[] = "Tire rotation";
     char wash[] = "Car wash";
     char wax[] = "Car wax";
-    char serviceOne_1[30];
-    char serviceTwo_1[30];
+    char serviceOne[30];
+    char serviceTwo[30];
     int oilValue = 35;
     int tireValue = 19;
     int washValue = 7;
     int waxValue = 12;
+    int total1 = 0;
+    int total2 = 0;
     printf("Davy's auto shop services\n Oil change -- $35\n Tire rotation -- $19\n Car wash -- $7\n Car wax -- $12\n");
     printf("\n");
     strcpy(oil, "Oil change");
@@ -41,21 +43,62 @@ int main()
     strcpy(wash, "Car wash");
     strcpy(wax, "Car wax");
     printf("Select first service:");
-    scanf ("%[^\n]%*c", serviceOne_1);
+    scanf ("%[^\n]%*c", serviceOne);
     printf("Select second service:");
-    scanf ("%[^\n]%*c", serviceTwo_1);
-        if(strcmp(serviceOne_1, oil) == 0)
+    scanf ("%[^\n]%*c", serviceTwo);
+        if(strcmp(serviceOne, oil) == 0)
     {
-    printf("Service 1: [%s], $%d\n", oil, oilValue);
-    }
+    printf("Service 1: %s, $%d\n", oil, oilValue);
+    total1 = oilValue;
+    }   
+        else if(strcmp(serviceOne, tire) == 0)
+     {
+     printf("Service 1: %s, $%d\n", tire, tireValue); 
+     total1 = tireValue;
+     }
+
+        else if(strcmp(serviceOne, wash) == 0)
+     {
+     printf("Service 1: %s, $%d\n", wash, washValue); 
+     total1 = washValue;
+     }
+        else if(strcmp(serviceOne, wax) == 0)
+     {
+     printf("Service 1: %s, $%d\n", wax, waxValue); 
+     total1 = waxValue;
+     }
     else 
     {
         printf("Service 1: No Service\n");
+        total1= 0;
     }
-    printf("[%s] $%d\n", oil, oilValue);
-    printf("[%s] $%d\n", tire, tireValue);
-    printf("[%s] $%d\n", wash, washValue);
-    printf("[%s] $%d\n", wax, waxValue);
+        if(strcmp(serviceTwo, oil) == 0)
+    {
+    printf("Service 2: %s, $%d\n", oil, oilValue);
+    total2 = oilValue;
+    }   
+        else if(strcmp(serviceTwo, tire) == 0)
+     {
+     printf("Service 2: %s, $%d\n", tire, tireValue); 
+     total2 = tireValue;
+     }
+
+        else if(strcmp(serviceTwo, wash) == 0)
+     {
+     printf("Service 2: %s, $%d\n", wash, washValue); 
+     total2 = washValue;
+     }
+        else if(strcmp(serviceTwo, wax) == 0)
+     {
+     printf("Service 2: %s, $%d\n", wax, waxValue); 
+     total2 = waxValue;
+     }
+    else 
+    {
+        printf("Service 2: No Service\n");
+        total2 = 0;
+    }
+    printf("Total: $%d\n", total1 + total2);
     return 0;
 }
 // Function Definitions
