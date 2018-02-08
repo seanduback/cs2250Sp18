999cd8c810dda915cb69a3b1772f0cb21ea110f9
seanduback <seandubck@mail.weber.edu>
2018-02-08 10:19:37 -0700

    Inital Commit

---
+++ mod5/hwMod5.c
@@ -0,0 +1,43 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  hwMod5.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  02/08/2018 10:11:18 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Sean Duback (), seanduback@mail.weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+
+// Constants
+
+// Function Prototypes
+
+// Main Function
+int main()
+{
+    char triangleChar = '-';
+    int triangleHeight = 0;
+    printf("Enter a character:\n");
+    scanf("%c", &triangleChar);
+    printf("Enter triangle height:\n");
+    scanf("%d", &triangleHeight);
+    // create a while statement 
+    while(int r = 0; r <= triangleHeight)
+    {
+        printf("%c\n", triangelChar);
+        r ++; 
+        
+    return 0;
+}
+// Function Definitions
+
+
