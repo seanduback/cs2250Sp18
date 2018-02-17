e5e6e6eb827aec838a8a718247807055b45dabe0
seanduback <seandubck@mail.weber.edu>
2018-02-17 16:19:31 -0700

    Fixed whitespaces v2

--- mod6/main.c
+++ mod6/main.c
@@ -47,7 +47,7 @@ int main()
     {
         printf("Player %d -- Jersey number: %d, Rating: %d\n", i, jearsyNum1[i], ratingVal1[i]);
     }
-    printf("\n");
+    printf("\n\n");
     while (choice != 'q')
     {
         printf("MENU\n");
@@ -56,7 +56,7 @@ int main()
         printf("r - Replace player\n");
         printf("o - Output roster\n");
         printf("q - Quit\n");
-        printf("\n");
+        printf("\n\n");
         printf("Choose an option:\n");
         scanf(" %c",&choice);
         switch (choice) 
@@ -84,6 +84,7 @@ int main()
             case 'a':
                 printf("Enter a rating\n");
                 scanf("%d", &ratingVal2);
+                printf("ABOVE %d\n", @ratingVal2);
 
                 for (int i=1; i<=5; i++){
                     if (ratingVal1[i] > ratingVal2)
