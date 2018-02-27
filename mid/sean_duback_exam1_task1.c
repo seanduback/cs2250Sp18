49497b49d2dae7cf8f1a110dddfc8ffc231410e6
seanduback <seandubck@mail.weber.edu>
2018-02-27 08:44:13 -0700

    inital commit

---
+++ mid/sean_duback_exam1_task1.c
@@ -0,0 +1,32 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  sean_duback_exam1_task1.c
+ *
+ *    Description: First choice   
+ *
+ *        Version:  1.0
+ *        Created:  02/27/2018 08:41:57 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Sean Duback (), seanduback@mail.weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+
+// Constants
+
+// Function Prototypes
+
+// Main Function
+int main()
+{
+
+    return 0;
+}
+// Function Definitions
+
+
---
+++ mid/sean_duback_exam1_task2.c
@@ -0,0 +1,32 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  sean_duback_exam1_task2.c
+ *
+ *    Description:  Second Choice 
+ *
+ *        Version:  1.0
+ *        Created:  02/27/2018 08:42:29 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Sean Duback (), seanduback@mail.weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+
+// Constants
+
+// Function Prototypes
+
+// Main Function
+int main()
+{
+
+    return 0;
+}
+// Function Definitions
+
+
