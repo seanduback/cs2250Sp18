fe9545a484b175b4c15a0f5776aea37a0817d29d
seanduback <seandubck@mail.weber.edu>
2018-04-08 17:14:02 -0600

    fixed, need: cammo after in, dataInt

--- mod11/hw/main2.c
+++ mod11/hw/main2.c
@@ -17,6 +17,7 @@
  */
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 // Main Function
 int main()
 {
@@ -27,24 +28,25 @@ int main()
     int j = 0;
     char dataPoint[50];
     char dataString[50][50];
-    int dataInt[50];
+    int dataInt[50][50];
     int counter = 0;
     int flag = 0;
     int comma = 0;
     printf("Enter a title for the data:\n");
     fgets (title, 50, stdin);
     title[strlen(title) - 1] = '\0';
-    printf("You entered: %s\n", title);
+    printf("You entered: %s\n\n", title);
     printf("Enter the column 1 header:\n");
     fgets (col1, 50, stdin);
     col1[strlen(col1) - 1] = '\0';
-    printf("You entered: %s\n", col1);
+    printf("You entered: %s\n\n", col1);
     printf("Enter the column 2 header:\n");
     fgets (col2, 50, stdin);
     col2[strlen(col2) - 1] = '\0';
-    printf("You entered: %s\n", col2);
+    printf("You entered: %s\n\n", col2);
     while (flag != 1) {
-        printf("Enter a data point(-1 to stop input): ");
+        comma = 0;
+        printf("Enter a data point (-1 to stop input):\n");
         fgets (dataPoint, 50, stdin);
         dataPoint[strlen(dataPoint) - 1] = '\0';
         if ((strcmp(dataPoint, "-1")) == 0) {
@@ -57,25 +59,32 @@ int main()
             }
             i++;
         }
-        i = 0;
-        if( dataPoint[i] != ',' && i < strlen(dataPoint)) {
+        int count = 0;
+        for( i = 0; dataPoint[i] != ','; i++) {
             dataString[j][i] = dataPoint[i];
-            i++;
+            count ++;
+        }
+        for ( i = count; dataPoint[i] != '\0'; i++){
+            if(isdigit(dataPoint[i]) == 0){
+                dataInt[j][i] = dataPoint[i];
+            }
         }
         dataString[j][i] = '\0';
         if (comma == 1){
-            char dummy[50];
-            sscanf(dataPoint, "%s %d",dummy,  &dataInt[j]);
+            // char dummy[50];
+            //  sscanf(dataPoint, "%s %d",dummy,  &dataInt[j]);
             printf("Data string: %s\n", dataString[j]);
-            printf("Data integer: %d\n", dataInt[j]);
+            printf("Data integer: %d\n", *dataInt[j]);
             counter ++;
             j ++;
             comma = 0;
+            i = 0;
         }
         else if ( comma == 0){
             printf("No comma in string.\n");
             flag = 0;
             i = 0;
+            comma = -1;
         }
         else if ( comma > 1){
             printf("Too many commas in input.\n");
@@ -93,14 +102,14 @@ int main()
         printf("--------------------------------------------\n");
         j = 0;
         while (j < counter){
-            printf("%-20s|%23d\n",dataString[j] , dataInt[j]);
+            printf("%-20s|%23d\n",dataString[j] , *dataInt[j]);
             j++;
         }
         printf("\n");
         j = 0;
         while (j < counter){
             printf("%20s ", dataString[j]);
-            while (i  < dataInt[j]){
+            while (i  < *dataInt[j]){
                 printf("*");
                 i++;
             }
