201932a0f719b2bfc74e64bcaa539cb33cdd6720
seanduback <seandubck@mail.weber.edu>
2018-04-08 13:39:09 -0600

    main.2 new final code, fourth commit

--- mod11/hw/main2.c
+++ mod11/hw/main2.c
@@ -20,27 +20,28 @@
 // Main Function
 int main()
 {
-    char dataString[50][50];
-    int dataInt[50];
     char col1[50];
     char col2[50];
     char title[50];
     int i = 0;
     int j = 0;
     char dataPoint[50];
+    char dataString[50][50];
+    int dataInt[50];
     int counter = 0;
     int flag = 0;
     printf("Enter a title for the data:\n");
     fgets (title, 50, stdin);
+    title[strlen(title) - 1] = '\0';
     printf("You entered: %s\n", title);
     printf("Enter the column 1 header:\n");
     fgets (col1, 50, stdin);
+    col1[strlen(col1) - 1] = '\0';
     printf("You entered: %s\n", col1);
     printf("Enter the column 2 header:\n");
     fgets (col2, 50, stdin);
-    printf("You entered: %s\n", col2);
-    col1[strlen(col1) - 1] = '\0';
     col2[strlen(col2) - 1] = '\0';
+    printf("You entered: %s\n", col2);
     while (flag != 1) {
         printf("Enter a data point(-1 to stop input): ");
         fgets (dataPoint, 50, stdin);
@@ -59,7 +60,7 @@ int main()
         printf("Data integer: %d\n", dataInt[j]);
         counter ++;
         j ++;
-        }
+    }
 
     if(flag ==  1){
         printf("\n\t\t\t\tFORMATTED TABLE\n");
