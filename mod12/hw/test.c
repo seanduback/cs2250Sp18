131f72c2cd823b9af932ffd86eec05a8acfa0603
seanduback <seandubck@mail.weber.edu>
2018-04-18 13:52:09 -0600

    inital commit

---
+++ mod12/hw/test.c
@@ -0,0 +1,34 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  test.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  04/18/2018 01:51:38 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Sean Duback (), seanduback@mail.weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+//#include <stdio.h> //For C
+#include <iostream>  //For C++
+using namespace std; //For C++
+
+// Constants
+
+// Function Prototypes
+
+// Main Function
+int main()
+{
+
+    return 0;
+}
+// Function Definitions
+
+
